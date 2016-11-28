b0df8f5e9e7b75c350776a96bce510e572f3d324
Jason Huang <cjhuang@yahoo-inc.com>
2016-11-28 17:02:59 +0800

    generate typeOrder

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,25 @@
 const fs = require('fs');
 const rimraf = require('rimraf');
-const csvFilePath = `${__dirname}/../data/types.csv`;
-const destFilePath = `${__dirname}/../dist/types.json`;
+const csvDir = `${__dirname}/../data`;
+const jsonDir = `${__dirname}/../dist`;
  
-const result = fs.readFileSync(csvFilePath);
-const types = result
+const result = fs.readFileSync(`${csvDir}/types.csv`);
+const lines = result
     .toString()
-    .split('\n')
+    .split('\n');
+
+const types = lines
     .slice(1)
     .map(line => line.trim().split(',').slice(1).map(Number));
 
-rimraf(`${__dirname}/../dist/`, () => {
-    fs.mkdirSync(`${__dirname}/../dist/`);
-    fs.writeFileSync(destFilePath, JSON.stringify(types, null, 4));
+const typeOrder = lines[0]
+    .trim()
+    .split(',')
+    .slice(1);
+
+rimraf(jsonDir, () => {
+    fs.mkdirSync(jsonDir);
+
+    fs.writeFileSync(`${jsonDir}/types.json`, JSON.stringify(types, null, 4));
+    fs.writeFileSync(`${jsonDir}/typeOrder.json`, JSON.stringify(typeOrder, null, 4));
 });
